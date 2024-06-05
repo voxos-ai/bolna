31aab0bb497d7170e846ea034fb5ddb62e2608b7
Marmik Pandya <marmikpandya@gmail.com>
2024-06-05 17:55:41 +0530

    add fillers

---
+++ bolna/classification/deberta.py
@@ -0,0 +1,30 @@
+
+
+from dotenv import load_dotenv
+from pathlib import Path
+from transformers import AutoTokenizer, pipeline
+from optimum.onnxruntime import ORTModelForSequenceClassification
+
+from bolna.classification.classification import BaseClassifier
+from bolna.helpers.logger_config import configure_logger
+
+
+logger = configure_logger(__name__)
+load_dotenv()
+    
+
+class DeBERTaClassifier(BaseClassifier):
+    def __init__(self, classifier, prompt, labels, threshold, multi_label=False, filename = None):
+        super().__init__(prompt, labels, multi_label, threshold)
+        self.model_args = { "model": self.model_name}
+        if filename is not None:
+            self.model_args['file_name'] = filename
+        self.model = ORTModelForSequenceClassification.from_pretrained(**self.model_args)
+        self.tokenizer = AutoTokenizer.from_pretrained(classifier)
+        self.tokenizer.model_input_names = ['input_ids', 'attention_mask']
+        self.classifier = pipeline("zero-shot-classification", model=self.model, tokenizer=self.tokenizer)
+        
+    def classify(self, text):
+        output = self.classifier(text, self.classification_labels, multi_label=self.multi_label)
+        return output['sequence']['labels'][0] if output['sequence']['labels'][0] > self.threshold else None
+
