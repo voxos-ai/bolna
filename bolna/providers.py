ed4ac9babe93a2ac2f6fd185902e23f8b4d72166
anshjoseph <ranshjosrph@gmail.com>
2024-06-06 19:17:13 +0530

    fix vllm

--- bolna/providers.py
+++ bolna/providers.py
@@ -27,7 +27,7 @@ SUPPORTED_LLM_PROVIDERS = {
     'fireworks': LiteLLM,
     'azure-openai': LiteLLM,
     'perplexity': LiteLLM,
-    'vllm': OpenAiLLM,
+    'vllm': LiteLLM,
     'anyscale': LiteLLM,
     'custom': OpenAiLLM,
     'ola': OpenAiLLM
