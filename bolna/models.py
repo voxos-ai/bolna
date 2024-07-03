9c4428e3d8c21c81d48e55b38f3bec357bb649b9
Marmik Pandya <marmikpandya@gmail.com>
2024-07-03 15:23:23 +0530

    backup

--- bolna/models.py
+++ bolna/models.py
@@ -227,7 +227,8 @@ class Task(BaseModel):
     tools_config: ToolsConfig
     toolchain: ToolsChainModel
     task_type: Optional[str] = "conversation"  # extraction, summarization, notification
-    task_config: ConversationConfig = dict()
+    task_config: Optional[ConversationConfig] = dict() #Just keeping it for backwards compatibility
+    conversation_config: ConversationConfig = dict()
     
 class AgentModel(BaseModel):
     agent_name: str
