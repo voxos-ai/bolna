d0f6158039eca5d02758cd9740199629a595957f
saurabhjdsingh <singhjdsaurabh@gmail.com>
2024-07-05 17:32:48 +0530

    daily fix test-2

--- bolna/agent_manager/assistant_manager.py
+++ bolna/agent_manager/assistant_manager.py
@@ -26,7 +26,9 @@ class AssistantManager(BaseManager):
         self.output_queue = output_queue
         self.kwargs = kwargs
         self.conversation_history = conversation_history
-        self.kwargs['agent_welcome_message'] = update_prompt_with_context(agent_config.get('agent_welcome_message', AGENT_WELCOME_MESSAGE), context_data)
+        logger.info("####")
+        self.kwargs['agent_welcome_message'] = agent_config.get('agent_welcome_message', AGENT_WELCOME_MESSAGE)
+        logger.info("####")
 
     async def run(self, local=False, run_id=None):
         """
